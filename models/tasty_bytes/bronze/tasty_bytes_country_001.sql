e51f20c135f686b0e498b4566753942e500af839
Arun Raja Anbalagan <arun.anbalagan@tigeranalytics.com>
2026-05-20 11:10:26 +0000

    Added tasty bytes source data

---
+++ models/tasty_bytes/bronze/tasty_bytes_country_001.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+            materialized = 'table',
+            database = var('tasty_bytes_db'),
+            schema = var('bronze_sch'),
+            alias = 'Country'
+
+    )
+}}
+
+
+SELECT
+    $1 AS country_id, 
+    $2 AS country,
+    $3 AS iso_currency, 
+    $4 AS iso_country, 
+    $5 AS city_id, 
+    $6 AS city, 
+    $7 AS city_population
+FROM @tb_101.public.s3load/raw_pos/country/
