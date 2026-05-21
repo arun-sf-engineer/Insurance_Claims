3cf82f5049da0d495ada970ac16577be8428d19a
Arun Raja Anbalagan <arun.anbalagan@tigeranalytics.com>
2026-05-21 08:23:59 +0000

    Committed location and franchise files

---
+++ models/tasty_bytes/bronze/tasty_bytes_menu_004.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+            materialized = 'table',
+            database = var('tasty_bytes_db'),
+            schema = var('bronze_sch'),
+            alias = 'menu',
+            tags = ['bronze']
+    )
+}}
+
+SELECT $1 AS menu_id,
+    $2 AS menu_type_id,
+    $3 AS menu_type,
+    $4 AS truck_brand_name,
+    $5 AS menu_item_id,
+    $6 AS menu_item_name,
+    $7 AS item_category,
+    $8 AS item_subcategory,
+    $9 AS cost_of_goods_usd,
+    $10 AS sale_price_usd,
+    $10 AS menu_item_health_metrics_obj
+FROM @tb_101.public.s3load/raw_pos/menu/
